d0d86efbd79d8c5a85a242906425d53236affd89
yeluriamrutha <lakshmiamruthayeluri@gmail.com>
2026-01-09 18:28:45 +0530

    feat(repo): add InMemoryRepository for tests

--- src/task_manager/repository.py
+++ src/task_manager/repository.py
@@ -12,3 +12,36 @@ class Repository(ABC):
     @abstractmethod
     def list_tasks(self) -> List[Task]:
         pass
+class InMemoryRepository:
+    def __init__(self):
+        self.tasks = {}
+        self.projects = {}
+
+    # ---- Task methods ----
+    def save_task(self, task):
+        self.tasks[task.id] = task
+
+    def get_task(self, task_id):
+        return self.tasks.get(task_id)
+
+    def list_tasks(self):
+        return list(self.tasks.values())
+
+    def delete_task(self, task_id):
+        self.tasks.pop(task_id, None)
+
+    # ---- Project methods ----
+    def save_project(self, project):
+        self.projects[project.id] = project
+
+    def get_project(self, project_id):
+        return self.projects.get(project_id)
+
+    def find_project_by_name(self, name):
+        for p in self.projects.values():
+            if p.name == name:
+                return p
+        return None
+
+    def delete_project(self, project_id):
+        self.projects.pop(project_id, None)
